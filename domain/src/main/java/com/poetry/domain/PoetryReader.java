00886ba01e9a91ccfaf9321557b998d03baf2be4
umavipplow <65022131+umavipplow@users.noreply.github.com>
2020-05-12 14:40:12 +0530

    refactor: extracted domain module from test module (#1)
    * KATA - 1 
    left side port for test for requesting poetry i.e.RequestVerse.
     domain i.e. PoetryReader which can be hardcoded. This very first version of the business logic returns an hard-coded result.
    
    * refactor: extracted domain module from test module

---
+++ domain/src/main/java/com/poetry/domain/PoetryReader.java
@@ -0,0 +1,8 @@
+package com.poetry.domain;
+
+public class PoetryReader implements RequestVerse {
+    @Override
+    public String giveMeSomePoetry() {
+        return "Early to Bed Early to  raise make a men  healthy  wealthy and wise";
+    }
+}
