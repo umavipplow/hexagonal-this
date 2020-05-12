d291b70aad2a732d7030fce965b244fcb700e814
umavipplow <65022131+umavipplow@users.noreply.github.com>
2020-05-12 16:43:54 +0530

    feat: added right side  port and obtaining poem using stub (#2)
    * feat: added right side  port and obtaining poem using stub
    
    * feat: added right side  port and obtaining poem using stub along with testcase

--- acceptance-test/src/test/java/com/poetry/domain/PoetryTest.java
+++ acceptance-test/src/test/java/com/poetry/domain/PoetryTest.java
@@ -6,11 +6,15 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 @RunWith(JUnitPlatform.class)
 public class PoetryTest {
+
 
     @Test
     @DisplayName("Should get verse when asked for poetry")
@@ -20,5 +24,14 @@ public class PoetryTest {
         RequestVerse poetryReader = new PoetryReader();
         String verse = poetryReader.giveMeSomePoetry();
         Assertions.assertEquals("Early to Bed Early to  raise make a men  healthy  wealthy and wise", verse);
+    }
+
+    @Test
+    @DisplayName("Should get verse when asked for poetry from stub")
+    public void testTheRightSidePort(@Mock ObtainPoem obtainPoem) {
+        when(obtainPoem.giveMePoetry()).thenReturn("Baa Baa Black sheep");
+        RequestVerse poetryReader = new PoetryReader(obtainPoem);
+        String verse = poetryReader.giveMeSomePoetry();
+        Assertions.assertEquals("Baa Baa Black sheep", verse);
     }
 }
---
+++ domain/src/main/java/com/poetry/domain/ObtainPoem.java
@@ -0,0 +1,7 @@
+package com.poetry.domain;
+
+public interface ObtainPoem {
+    default String giveMePoetry() {
+        return "Early to Bed Early to  raise make a men  healthy  wealthy and wise";
+    }
+}
