d2d920f0c364ce322c2005e5164e777b1ec61e0c
Farzain <farzainpathan@gmail.com>
2020-12-09 13:08:09 +0530

    feat: get verse from ObtainPoem

--- domain/src/main/java/com/potery/PoetryReader.java
+++ domain/src/main/java/com/potery/PoetryReader.java
@@ -1,11 +1,23 @@
 package com.potery;
 
+import com.potery.port.ObtainPoem;
 import com.potery.port.RequestVerse;
 
 public class PoetryReader implements RequestVerse {
 
+    ObtainPoem obtainPoem;
+
+    public PoetryReader() {
+        this.obtainPoem = new ObtainPoem() {
+        };
+    }
+
+    public PoetryReader(ObtainPoem obtainPoem) {
+        this.obtainPoem = obtainPoem;
+    }
+
     @Override
     public String getPoetry() {
-        return "Jack and Jill";
+        return obtainPoem.getPoem();
     }
 }
